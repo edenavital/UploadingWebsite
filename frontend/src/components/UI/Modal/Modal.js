a22ee4ba21a5e846781233e4ae51313af7f45fd9
edenavital <edendor@gmail.com>
2019-10-25 14:51:57 +0300

    Refactoring variables names and implemented good code practises

--- frontend/src/components/UI/Modal/Modal.js
+++ frontend/src/components/UI/Modal/Modal.js
@@ -2,30 +2,37 @@ import React from "react";
 import "./Modal.css";
 import Backdrop from "../Backdrop/Backdrop";
 
-//Modal props: showModal, closedModalHandler(), message
-// showModal - For Backdrop & Modal components - boolean
-// closedModalHandler() - Changes the showModal prop to false
-// type - Indicates a Successful upload or Error message
+//props: isModalVisible, closedModalHandler(), isModalPositive
+// isModalVisible - The whole Modal appears according to this boolean
+// closedModalHandler() - Changes the isModalVisible prop to false - Which hides the Modal & Backdrop
+// isModalPositive - Indicates a Successful upload or an Error message
 
-const Modal = props => (
-  <>
-    <Backdrop showModal={props.showModal} clicked={props.closeModalHandler} />
-    <div
-      className="Modal"
-      style={{
-        transform: props.showModal ? "translateY(0)" : "translateY(-100vh)",
-        opacity: props.showModal ? "1" : "0"
-      }}
-    >
-      {props.type ? (
-        <h4 style={{ color: "green" }}>
-          The image has been successfully uploaded!
-        </h4>
-      ) : (
-        <h4 style={{ color: "red" }}>The selected file is not an image!</h4>
-      )}
-    </div>
-  </>
-);
+//TASK:
+// Change isModalPositive to ModalType string, add a switch case for modalType === "green" || "red" || "image"
+// In the MediaItems component, generate new states and import Modal component, use it when clicking on one of the MediaItem components
+// change 'message' variable to 'content'
 
+const Modal = props => {
+  //Animates the Modal Pop up by toggling class 'On'
+  let className = props.isModalVisible ? "Modal On" : "Modal";
+
+  //Modal's message to the user, depending on the prop.isModalPositive
+  let message = props.isModalPositive ? (
+    <h4 style={{ color: "green" }}>
+      The image has been successfully uploaded!
+    </h4>
+  ) : (
+    <h4 style={{ color: "red" }}>The selected file is not an image!</h4>
+  );
+
+  return (
+    <>
+      <Backdrop
+        isModalVisible={props.isModalVisible}
+        clicked={props.closeModalHandler}
+      />
+      <div className={className}>{message}</div>
+    </>
+  );
+};
 export default Modal;
