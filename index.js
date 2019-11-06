60bc4fe5150de2b30fbd60f01a8ea020d159384b
edenavital <edendor@gmail.com>
2019-11-06 22:53:55 +0200

    fix

--- index.js
+++ index.js
@@ -95,12 +95,10 @@ app.delete("/api/media/:id", (req, res, next) => {
   res.status(204).send();
 });
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
