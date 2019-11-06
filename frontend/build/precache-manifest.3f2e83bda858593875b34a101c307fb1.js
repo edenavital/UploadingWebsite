792e80692dc7de5338fd11511080a3551b8b3818
edenavital <edendor@gmail.com>
2019-11-06 22:50:08 +0200

    fix

---
+++ frontend/build/precache-manifest.3f2e83bda858593875b34a101c307fb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cbc84e5aa4d4d712264777a2da6ce2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4df3ebda1d3ccf77e9bd",
+    "url": "/static/css/main.4a0422c1.chunk.css"
+  },
+  {
+    "revision": "51a65dbeb445594bb166",
+    "url": "/static/js/2.9ad614de.chunk.js"
+  },
+  {
+    "revision": "4df3ebda1d3ccf77e9bd",
+    "url": "/static/js/main.80b9cc18.chunk.js"
+  },
+  {
+    "revision": "bf1ed1e30c45dc2ee2e8",
+    "url": "/static/js/runtime-main.7b7879bd.js"
+  },
+  {
+    "revision": "ef66d3fd990a02b10dc5d9ab5762ae3a",
+    "url": "/static/media/homepage-background.ef66d3fd.jpg"
+  }
+]);
