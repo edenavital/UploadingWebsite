b62e06cb5140e061b7eaf132c38ae42b63fcc3ae
edenavital <edendor@gmail.com>
2019-11-06 22:46:27 +0200

    ll

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
 // Tasks: Add a Spinner when requesting data from the server - V
@@ -103,4 +103,4 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
