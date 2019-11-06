ba8c2a27ce877e6f6e94ef54cf3e72259972868b
edenavital <edendor@gmail.com>
2019-11-06 22:13:36 +0200

    test

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "public", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
 
