69fffaf6545170035703cda1d7122c787bada6ad
Eden <edendor@gmail.com>
2019-12-12 14:24:17 +0200

    Changed enlarge functionality && MediaItem css

--- frontend/src/components/MediaItem/MediaItem.tsx
+++ frontend/src/components/MediaItem/MediaItem.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import "./MediaItem.css";
 import DeleteButton from "../DeleteButton/DeleteButton";
 //props: id, name, path, deleteImageHandler(id), enlargeImageHandler(path)
-import EnlargedImage from "../EnlargedImage/EnlargedImage";
 
 interface Props {
   deleteImageHandler: (id: number) => void;
@@ -19,9 +18,8 @@ const MediaItem: React.FC<Props> = ({
   id,
   name
 }) => (
-  <div className="MediaItem">
+  <div className="MediaItem" onClick={() => enlargeImageHandler(path)} >
     <DeleteButton deleteImageHandler={() => deleteImageHandler(id)} />
-    <EnlargedImage enlargeImageHandler={() => enlargeImageHandler(path)} />
     <img src={path} alt="Media" />
     <h4>{name}</h4>
   </div>
